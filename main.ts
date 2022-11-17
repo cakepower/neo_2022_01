a88f0aba562129644e8dabcf71a06856af810c6c
cakepower <32028127+cakepower@users.noreply.github.com>
2022-11-17 10:14:48 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -11,13 +11,22 @@ input.onButtonPressed(Button.A, function () {
 })
 input.onButtonPressed(Button.B, function () {
     strip.setBrightness(50)
-    for (let index = 0; index <= 24; index++) {
-        strip.setPixelColor(index, neopixel.rgb(0, index * 10, 0))
+    for (let index = 0; index <= 25; index++) {
+        if (index1 % 3 == 0) {
+            strip.setPixelColor(index, neopixel.rgb(0, 255, 0))
+        } else if (index1 % 3 == 1) {
+            strip.setPixelColor(index, neopixel.rgb(255, 0, 0))
+        } else if (index1 % 3 == 2) {
+            strip.setPixelColor(index, neopixel.rgb(0, 255, 255))
+        }
         strip.show()
         basic.pause(100)
     }
     strip.showColor(neopixel.colors(NeoPixelColors.Black))
     strip.show()
+    index1 += 1
 })
+let index1 = 0
 let strip: neopixel.Strip = null
 strip = neopixel.create(DigitalPin.P13, 24, NeoPixelMode.RGB)
+index1 = 0
